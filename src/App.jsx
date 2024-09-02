d3e36794a147bd6e0e6b3f3a82f63b5c0820f5da
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-02 19:26:18 +0800

    Add Navbar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import {
   Footer 
 } from './sections';
 
+import Nav from './components/nav';
+
 
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
