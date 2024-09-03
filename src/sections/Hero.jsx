3d60d957c423b48391ebb5847d4a489ccf96c823
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-03 23:01:43 +0800

    Adding the Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,37 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
+    >
+      <div className="relative  xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="div-above-the-heading">Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innocation for
+          your active life.
+        </p>
+        <Button label="Show now" iconUrl={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
