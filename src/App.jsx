eb96332f1a0e592e7f0874d293db70431d0f8859
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-03 23:29:43 +0800

    finalize Hero

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Nav from './components/nav';
 const App = () => (
   <main className="relative">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding"><PopularProducts /></section>
