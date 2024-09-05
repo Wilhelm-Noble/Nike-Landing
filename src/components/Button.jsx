93fc17e3f5a5fd4088a4480746f31c20be94befe
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-06 02:46:07 +0800

    Update Button and finalize Subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,28 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className=" rounded-full text-white flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red">
-        {label} 
-    <img src={iconUrl} alt='right arrow' className="ml-2 rounded-full w-5 h-5" />
-    </button>
-  )
-}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
 
-export default Button
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
