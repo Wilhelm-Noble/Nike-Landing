f2c18d3ebe7e3c30c944bdfe5a4a47e71dd3c3aa
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-06 02:17:13 +0800

    Special Offer Initial

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,21 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img
+          src={offer}
+          alt='Shoe Promotion'
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
