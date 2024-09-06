535a82cdb20c34a123abf47cb1a0ff16e5d797c6
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-07 01:57:21 +0800

    initial on Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,24 @@
+import {footerLogo} from '../assets/images'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+          <img src={footerLogo} width={150} height={76} alt="footer" />
+          <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>Get shoes ready for the new term at your nearest Nike store. Find Your perfect Size In Store. Get Rewards</p>
+          <div className='flex items-center gap-5 mt-8'>
+            {socialMedia.map((icon) => (
+              <div className=''>
+                <img src={icon.src} alt={icon.alt} width={24} height={24}/>
+              </div>
+            ))}
+          </div>
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
